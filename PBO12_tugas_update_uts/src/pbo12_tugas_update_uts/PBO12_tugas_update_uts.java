1ff6cdfd8fb357088baf3ab07b97699679506256
RifanHidayat <rifanhidayat0811@gmail.com>
2017-11-30 13:55:39 +0700

    Add files via upload

---
+++ PBO12_tugas_update_uts/src/pbo12_tugas_update_uts/PBO12_tugas_update_uts.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo12_tugas_update_uts;
+
+class RegistrasiAnggotaGym {
+
+    private String no_ktp;
+    private String nama;
+    private String JK, cabang, PF, handuk;
+
+    public String Getktp() {
+
+        return no_ktp;
+    }
+
+    public void Setnoktp(String no_ktp) {
+        this.no_ktp = no_ktp;
+
+    }
+
+    public String Getnama() {
+
+        return nama;
+    }
+
+    public void Setnama(String nama) {
+        this.nama = nama;
+
+    }
+
+    public String GetJK() {
+
+        return JK;
+    }
+
+    public void SetJK(String JK) {
+
+        this.JK = JK;
+
+    }
+
+    public String Gehanduk() {
+
+        return no_ktp;
+    }
+
+    public void Sethanduk(String no_ktp) {
+        this.no_ktp = no_ktp;
+
+    }
+
+    public String GetPF() {
+
+        return PF;
+    }
+
+    public void SetPF(String PF) {
+        this.PF = PF;
+
+    }
+
+}
+
+public class PBO12_tugas_update_uts {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
